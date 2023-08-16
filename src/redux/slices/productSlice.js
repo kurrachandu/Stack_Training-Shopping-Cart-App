492063995d037621e137457633b9b58c4bc8b814
kurra chandu <nanic6465@gmail.com>
2023-08-16 19:01:27 +0530

    productSlice.js

--- src/redux/slices/productSlice.js
+++ src/redux/slices/productSlice.js
@@ -1,6 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const productSlice = createSlice 
+const productSlice = createSlice ({
+    name : "products",
+    initialState : [],
+    reducers : {
+        
+    }
+});
 
 export const { setProducts, setSearchKeyword } = productSlice.actions;
 export default productSlice.reducer;
